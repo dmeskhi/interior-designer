a50ac602d65ac0127a763f7f36e7bc999833f7fd
David Meskhishvili <dmeskh@gmail.com>
2024-08-02 23:06:25 -0700

    Finish styling data section

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector(".menu-icon");
 const xBtn = document.querySelector(".x-btn");
 const navigation = document.querySelector(".navigation");
+let start = false;
 
 
 menuIcon.addEventListener("click", () => {
@@ -15,6 +16,8 @@ window.addEventListener("scroll", () => {
     const about = document.querySelector(".about");
     const services = document.querySelector(".services");
     const portfolio = document.querySelector(".portfolio");
+    const data = document.querySelector(".data");
+    const numS = document.querySelectorAll(".num");
 
     if (window.scrollY >= 200) {
         about.classList.add("change");
@@ -33,4 +36,23 @@ window.addEventListener("scroll", () => {
       } else {
         portfolio.classList.remove("change");
       }
+
+      if (window.scrollY >= data.offsetTop - 300) {
+        if(!start) {
+            numS.forEach((num) => {
+                startCount(num);
+            });
+        }
+        start = true;
+      }
 });
+
+const startCount = el => {
+    let max = el.dataset.val;
+    let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent === max) {
+            clearInterval(count);
+        }
+    }, 5)
+}

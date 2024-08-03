9a230746699d0e24932e287823c758e42ea69435
David Meskhishvili <dmeskh@gmail.com>
2024-08-02 20:26:31 -0700

    Finish styling about section

---
+++ script.js
@@ -0,0 +1,24 @@
+const menuIcon = document.querySelector(".menu-icon");
+const xBtn = document.querySelector(".x-btn");
+const navigation = document.querySelector(".navigation");
+
+
+menuIcon.addEventListener("click", () => {
+    navigation.classList.add("navigate");
+});
+
+xBtn.addEventListener("click", () => {
+    navigation.classList.remove("navigate");
+});
+
+window.addEventListener("scroll", () => {
+    const about = document.querySelector(".about");
+    
+    if (window.scrollY >= 200) {
+        about.classList.add("change");
+      } else {
+        about.classList.remove("change");
+      }
+
+      console.log(window.scrollY);
+});
